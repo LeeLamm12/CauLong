fb4249b704778c18b65eb9922c76dd1f8c95958c
ASUS <ASUS@192.168.1.51>
2026-04-24 12:51:45 +0700

    Fix: add source code

---
+++ src/main/java/com/mycompany/quanlyshopdocaulong/QuanlyShopdoCauLong.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlyshopdocaulong;
+
+/**
+ *
+ * @author ASUS
+ */
+public class QuanlyShopdoCauLong {
+
+    public static void main(String[] args) {
+        System.out.println("Test Lan 2");
+    }
+}
